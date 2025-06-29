1492f743a84ec275fdf5373da4dba96ecb87fcf0
jeremymarch <jmarch@gradcenter.cuny.edu>
2025-06-28 23:08:42 -0400

    updates; add labels

--- src/lib.rs
+++ src/lib.rs
@@ -123,7 +123,7 @@ pub fn create_table2() {
                     3 => td.set_inner_html("9:30 a.m."),
                     4 => td.set_inner_html("10:40 a.m."),
                     5 => td.set_inner_html("12:15 p.m."),
-                    6 => td.set_inner_html("1:00 p.m. -------- "),
+                    6 => td.set_inner_html("1:00 p.m. ---------- "),
                     7 => td.set_inner_html(" ---------- 4:00 p.m."),
                     8 => td.set_inner_html("stats"),
                     _ => (),
@@ -135,66 +135,136 @@ pub fn create_table2() {
         let tr = document.create_element("tr").unwrap();
         tr.set_class_name(get_weekday(row.date.weekday()).to_lowercase().as_str());
         table.append_child(&tr).unwrap();
-        for col in 0..9 {
-            let td = document.create_element("td").unwrap();
-            tr.append_child(&td).unwrap();
-            match col {
-                0 => {
-                    td.set_class_name("quizcolumn");
-                    td.set_inner_html(row.quiz_grader.as_str());
-                }
-                1 => {
-                    let day = format!(
-                        "{}<br>{}{}",
-                        get_weekday(row.date.weekday()),
-                        row.date.strftime("%B %-d").to_string(),
-                        if row.day.is_empty() {
-                            String::from("")
-                        } else {
-                            format!("<br>Day {}", row.day)
-                        }
-                    );
-                    td.set_inner_html(&day);
-                    td.set_class_name("daycolumn");
-                }
-                2 => {
-                    td.set_class_name("morningoptionalcolumn");
-                    td.set_inner_html(
-                        &row.morning_optional
-                            .clone()
-                            .unwrap_or_else(|| "".to_string()),
-                    );
-                }
-                3 => {
-                    td.set_class_name("drill1column");
-                    td.set_inner_html(get_drill_col(&row.drill1).as_str());
-                }
-                // quizzes
-                // find self-correcting
-                // 7-10 schedule
-                4 => {
-                    td.set_class_name("drill2column");
-                    td.set_inner_html(get_drill_col(&row.drill2).as_str());
-                }
-                5 => {
-                    td.set_class_name("noonoptionalcolumn");
-                    td.set_inner_html(&row.noon_optional1.clone());
-                }
-                6 => {
-                    td.set_class_name("lecturecolumn");
-                    td.set_inner_html(&row.lecture.clone());
-                }
-                7 => {
-                    td.set_class_name("vocnotescolumn");
-                    td.set_inner_html(&row.voc_notes.clone());
-                }
-                8 => td.set_class_name("statscolumn"),
 
-                _ => (),
+        if row.exam.is_some() {
+            for col in 0..6 {
+                match col {
+                    0 => {
+                        let td = document.create_element("td").unwrap();
+                        tr.append_child(&td).unwrap();
+                    }
+                    1 => {
+                        let td = document.create_element("td").unwrap();
+
+                        let day = format!(
+                            "{}<br>{}{}",
+                            get_weekday(row.date.weekday()),
+                            row.date.strftime("%B %-d").to_string(),
+                            if row.day < 1 {
+                                String::from("")
+                            } else {
+                                format!("<br>Day {}", row.day)
+                            }
+                        );
+                        td.set_inner_html(&day);
+                        td.set_class_name("daycolumn");
+
+                        tr.append_child(&td).unwrap();
+                    }
+                    2 => {
+                        let td = document.create_element("td").unwrap();
+                        let _ = td.set_attribute("colspan", "4");
+                        td.set_inner_html("JM");
+                        tr.append_child(&td).unwrap();
+                    }
+                    3 => {
+                        let td = document.create_element("td").unwrap();
+                        td.set_class_name("lecturecolumn");
+                        let lecture_str = format!("{}<br>{}", row.lecture_title, row.lecture);
+                        td.set_inner_html(&lecture_str);
+                        tr.append_child(&td).unwrap();
+                    }
+                    4 => {
+                        let td = document.create_element("td").unwrap();
+                        let v = format!("Vocabulary Notes<br>{}", row.voc_notes.clone());
+                        td.set_inner_html(&v);
+                        tr.append_child(&td).unwrap();
+                    }
+                    5 => {
+                        let td = document.create_element("td").unwrap();
+                        tr.append_child(&td).unwrap();
+                    }
+
+                    _ => (),
+                }
             }
-            if col == 2 && row.day.is_empty() {
-                let _ = td.set_attribute("colspan", "8");
-                break;
+        } else {
+            for col in 0..9 {
+                let td = document.create_element("td").unwrap();
+                tr.append_child(&td).unwrap();
+                match col {
+                    0 => {
+                        td.set_class_name("quizcolumn");
+                        td.set_inner_html(row.quiz_grader.as_str());
+                    }
+                    1 => {
+                        let day = format!(
+                            "{}<br>{}{}",
+                            get_weekday(row.date.weekday()),
+                            row.date.strftime("%B %-d").to_string(),
+                            if row.day < 1 {
+                                String::from("")
+                            } else {
+                                format!("<br>Day {}", row.day)
+                            }
+                        );
+                        td.set_inner_html(&day);
+                        td.set_class_name("daycolumn");
+                    }
+                    2 => {
+                        td.set_class_name("morningoptionalcolumn");
+                        if let Some(s) = row.morning_optional.clone() {
+                            let s = format!("(optional)<br>{}", s);
+                            td.set_inner_html(&s);
+                        }
+                    }
+                    3 => {
+                        td.set_class_name("drill1column");
+                        td.set_inner_html(get_drill_col(&row.drill1).as_str());
+                    }
+                    // quizzes
+                    // find self-correcting
+                    // 7-10 schedule
+                    4 => {
+                        td.set_class_name("drill2column");
+                        td.set_inner_html(get_drill_col(&row.drill2).as_str());
+                    }
+                    5 => {
+                        td.set_class_name("noonoptionalcolumn");
+                        td.set_inner_html(&row.noon_optional1.clone());
+                    }
+                    6 => {
+                        if row.friday_review1.len() > 0 {
+                            td.set_class_name("fridayreviewcolumn1");
+                            td.set_inner_html(
+                                get_review_col(&row.friday_review1, row.day).as_str(),
+                            );
+                        } else {
+                            td.set_class_name("lecturecolumn");
+                            let lecture_str = format!("{}<br>{}", row.lecture_title, row.lecture);
+                            td.set_inner_html(&lecture_str);
+                        }
+                    }
+                    7 => {
+                        if row.friday_review2.len() > 0 {
+                            td.set_class_name("fridayreviewcolumn1");
+                            td.set_inner_html(
+                                get_review_col(&row.friday_review2, row.day).as_str(),
+                            );
+                        } else {
+                            td.set_class_name("vocnotescolumn");
+                            let v = format!("Vocabulary Notes<br>{}", row.voc_notes.clone());
+                            td.set_inner_html(&v);
+                        }
+                    }
+                    8 => td.set_class_name("statscolumn"),
+
+                    _ => (),
+                }
+                if col == 2 && row.day < 1 {
+                    let _ = td.set_attribute("colspan", "8");
+                    break;
+                }
             }
         }
     }
@@ -205,6 +275,14 @@ fn get_drill_col(fac: &Vec<String>) -> String {
         2 => format!("E - {}<br>F - {}", fac[0], fac[1]),
         3 => format!("E - {}<br>F/G - {}<br>H - {}", fac[0], fac[1], fac[2]),
         _ => String::from(""),
+    }
+}
+
+fn get_review_col(fac: &Vec<String>, day: u32) -> String {
+    if day < 15 {
+        format!("Review<br>E/F - {}<br>G/H - {}", fac[0], fac[1])
+    } else {
+        format!("Review<br>E - {}<br>F - {}", fac[0], fac[1])
     }
 }
 
